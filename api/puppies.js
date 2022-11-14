4684194a2e6ad1fb3fe9bab272e13591735574e6
Camden Jenkins <camdenjenkins@yahoo.com>
2022-11-14 10:35:07 -0800

    server connected

---
+++ api/puppies.js
@@ -0,0 +1,13 @@
+const router = require("express").Router();
+const { asyncErrorHandler } = require("./utils");
+const prisma = require("../prisma/prisma");
+//get /api/puppies
+router.get(
+  "/",
+  asyncErrorHandler(async (req, res, next) => {
+    const puppies = await prisma.puppies.findMany();
+    res.send(puppies);
+  })
+);
+
+module.exports = router;
