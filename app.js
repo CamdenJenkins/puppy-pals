4cc3bcc4b83167381411749244d8b7e3be64713e
Camden Jenkins <camdenjenkins@yahoo.com>
2022-11-14 10:01:47 -0800

    server initialized

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require("express");
+
+const app = express();
+const morgan = require("morgan");
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(morgan("dev"));
+
+app.get("/health", (req, res) => {
+  res.send("All healthy here!");
+});
+
+//api routes
+app.get("*", (req, res, next) => {
+  res.send(`Oops can't find that route!`);
+});
+
+app.use((error, req, res, next) => {
+  res.status(500).send(error);
+});
+
+module.exports = app;
