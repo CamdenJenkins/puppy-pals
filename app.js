8d77db17a46a191baf5d80cabd764d4ecf0929e1
Camden Jenkins <camdenjenkins@yahoo.com>
2022-11-16 08:43:17 -0800

    react app init

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const express = require("express");
-
+const path = require("path");
 const app = express();
 const morgan = require("morgan");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(morgan("dev"));
+app.use(express.static(path.join(__dirname, "./client", "dist")));
 
 app.get("/health", (req, res) => {
   res.send("All healthy here!");
@@ -15,8 +16,8 @@ app.get("/health", (req, res) => {
 
 app.use("/api", require("./api"));
 
-app.get("*", (req, res, next) => {
-  res.status(404).send(`Oops can't find that route!`);
+app.use((req, res, next) => {
+  res.sendFile(path.join(__dirname, "./client/dist", "index.html"));
 });
 
 app.use((error, req, res, next) => {
